388231292fb6f09863c1dbe110128f48f9b19f5e
Kulicod <fridman1986@gmail.com>
2023-06-14 19:31:00 +0300

    Cipher CW

--- src/Shape.ts
+++
@@ -1,4 +0,0 @@
-export default interface Shape {
-    perimeter(): number;
-    square(): number;
-}
---
+++ src/model/CipherType.ts
@@ -0,0 +1,5 @@
+type CypherType = {
+    key: string;
+    cipherText: string;
+}
+export default CypherType;
